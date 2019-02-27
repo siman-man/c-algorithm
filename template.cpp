2e742ac26a0716c233a4d2b1fbfcfcbc53e278f0
Shuichi Tamayose <tmshuichi@gmail.com>
2019-02-27 18:49:09 +0900

    Add template

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <queue>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+
+  cout << fixed << setprecision(10) << 0.12 << endl;
+
+  return 0;
+}

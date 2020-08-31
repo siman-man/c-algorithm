2d8b0430428aef8000c3d022ec02cda892908aef
Shuichi Tamayose <tmshuichi@gmail.com>
2020-09-01 07:20:09 +0900

    Add suffix_array.cpp

---
+++ string/suffix_array.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+typedef long long ll;
+
+const int MAX_N = 500000;
+
+int n, k;
+int _rank[MAX_N + 1];
+int tmp[MAX_N + 1];
+
+bool compare_sa(int i, int j) {
+  if (_rank[i] != _rank[j]) {
+    return _rank[i] < _rank[j];
+  } else {
+    int ri = i + k <= n ? _rank[i + k] : -1;
+    int rj = j + k <= n ? _rank[j + k] : -1;
+
+    return ri < rj;
+  }
+}
+
+void construct_sa(string S, int *sa) {
+  n = S.size();
+
+  for (int i = 0; i <= n; ++i) {
+    sa[i] = i;
+    _rank[i] = i < n ? S[i] : -1;
+  }
+
+  for (k = 1; k <= n; k *= 2) {
+    sort(sa, sa + n + 1, compare_sa);
+
+    tmp[sa[0]] = 0;
+    for (int i = 1; i <= n; ++i) {
+      tmp[sa[i]] = tmp[sa[i - 1]] + (compare_sa(sa[i - 1], sa[i]) ? 1 : 0);
+    }
+    for (int i = 0; i <= n; ++i) {
+      _rank[i] = tmp[i];
+    }
+  }
+}
+
+int main() {
+  string S;
+  cin >> S;
+  int N = S.size();
+  int sa[N + 1];
+
+  construct_sa(S, sa);
+
+  for (int i = 1; i <= n; ++i) {
+    cout << sa[i];
+
+    if (i == n) {
+      cout << endl;
+    } else {
+      cout << " ";
+    }
+  }
+
+  return 0;
+}

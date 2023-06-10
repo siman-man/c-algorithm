9be4ebfedb94178419b4af519d805b7011bfc9f1
Shuichi Tamayose <tmshuichi@gmail.com>
2023-06-10 17:23:12 +0900

    Add ternary_search_tree.cpp

---
+++ ternary_search_tree.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <limits.h>
+#include <map>
+#include <queue>
+#include <set>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+bool f(double v) {
+}
+
+int main() {
+  int loop_cnt = 1000;
+  double left = -10'000'000;
+  double right = 10'000'000;
+
+  for (int i = 0; i < loop_cnt; ++i) {
+  }
+
+  return 0;
+}

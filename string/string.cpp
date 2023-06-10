a56a234311adbe3e082991a11190d87e4e14b385
Shuichi Tamayose <tmshuichi@gmail.com>
2023-06-10 17:02:53 +0900

    Update file-struct

--- string/string.cpp
+++ string/string.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 typedef long long ll;
 
-const int MAX_N = 500000;
+const int MAX_N = 500'000;
 
 int n, k;
 int _rank[MAX_N + 1];
@@ -39,6 +39,26 @@ void construct_sa(string S, int *sa) {
     for (int i = 0; i <= n; ++i) {
       _rank[i] = tmp[i];
     }
+  }
+}
+
+void construct_lcp(string S, int *sa, int *lcp) {
+  int n = S.size();
+  for (int i = 0; i <= n; ++i) {
+    _rank[sa[i]] = i;
+  }
+
+  int h = 0;
+  lcp[0] = 0;
+  for (int i = 0; i < n; ++i) {
+    int j = sa[_rank[i] - 1];
+
+    if (h > 0) --h;
+    for (; j + h < n && i + h < n; ++h) {
+      if (S[j + h] != S[i + h]) break;
+    }
+
+    lcp[_rank[i] - 1] = h;
   }
 }
 
